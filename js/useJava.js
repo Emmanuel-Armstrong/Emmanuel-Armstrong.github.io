8f083a6a0bb01f5f3897096d186a99e660ee6534
Emmanuel-Armstrong <emmanuelrarmstrong@gmail.com>
2018-08-07 08:02:01 -0600

    Update useJava.js

--- js/useJava.js
+++ js/useJava.js
@@ -2,35 +2,9 @@
 
 var pflApp = Java.type("pflorderingproblem.UI");
 
-var firstName;
-var lastName;
-var companyName;
-var address1;
-var address2;
-var city;
-var state;
-var postalCode;
-var countryCode;
-var email;
-var phoneNumber;
-
 function getProducts(){  
   if (pflApp !== null){
     pflApp.getPackages();
     var products = pflApp.products;
   }
-}
-
-function setCustomer(){  
-    firstName = document.getElementById("first_name").innerHTML;
-    lastName = document.getElementById("last_name").innerHTML;
-    companyName = document.getElementById("company").innerHTML;
-    address1 = document.getElementById("street_address").innerHTML;
-    address2 = document.getElementById("street_address2").innerHTML;
-    city = document.getElementById("city").innerHTML;
-    state = document.getElementById("state").innerHTML;
-    postalCode = document.getElementById("zipCode").innerHTML;
-    countryCode = document.getElementById("country").innerHTML;
-    email = document.getElementById("email").innerHTML;
-    phoneNumber = document.getElementById("phone_number").innerHTML; 
 }
